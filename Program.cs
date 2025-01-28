7cbcef432a8f18cc208ec1aa98ca45c8e0cedfcc
Ajitha <ajithagollapudi2003@gmail.com>
2025-01-28 11:13:42 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        string url = "https://www.example.com";
+        string content = await FetchContentAsync(url);
+        Console.WriteLine(content);
+    }
+
+    static async Task<string> FetchContentAsync(string url)
+    {
+        using HttpClient client = new HttpClient();
+        string result = await client.GetStringAsync(url);
+        return result;
+    }
+}
+
